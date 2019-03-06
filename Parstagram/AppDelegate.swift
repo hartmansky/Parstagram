7726df1def2a071e32be6cb2e4fcd6c49e5e1c85
Sky Hartman <47018249+hartmansky@users.noreply.github.com>
2019-03-05 22:44:48 -0800

    Parstagram part1

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -17,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Parstagram"
+                configuration.server = "https://stark-springs-65022.herokuapp.com/parse"
+            })
+        )
+        
         return true
     }
 

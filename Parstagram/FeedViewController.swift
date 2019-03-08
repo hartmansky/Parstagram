3038064e5f3df066de7870d680d4334a4ff9b03b
Sky Hartman <47018249+hartmansky@users.noreply.github.com>
2019-03-07 20:56:00 -0800

    Parstagram part2

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 import Parse
 import AlamofireImage
+import MessageInputBar
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    let commentBar = MessageInputBar()
     
     var posts = [PFObject]()
     
@@ -22,6 +24,14 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.delegate = self
         tableView.dataSource = self
         // Do any additional setup after loading the view.
+    }
+    
+    override var inputAccessoryView: UIView?{
+        return commentBar
+    }
+    
+    override var canBecomeFirstResponder: Bool{
+        return true
     }
     
     override func viewDidAppear(_ animated: Bool) {
